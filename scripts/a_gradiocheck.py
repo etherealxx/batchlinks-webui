46e54902d76110c78d438cb2439d3838a2d1af26
Etherealxx <gwathon3@gmail.com>
2023-03-04 11:17:31 +0700

    🍫Progress Bar for queue on and 📴Disable when queue off

---
+++ scripts/a_gradiocheck.py
@@ -0,0 +1,27 @@
+from modules.shared import cmd_opts
+import os, inspect, re #, sys, shlex
+
+script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+batchlinks_dir = os.path.join(script_dir, "batchlinks-downloader.py")
+
+# remove the progress bar if queue off
+with open(batchlinks_dir, 'r') as f:
+    contents = f.read()
+
+if cmd_opts.gradio_queue:
+    #os.system(f"sed -i 's/def run(command, choosedowner):/def run(command, choosedowner, progress=gr.Progress()):/g' {batchlinks_dir}")
+    # os.system(f"sed -i '/^#progress/s/^#//' {batchlinks_dir}")
+    new_contents = re.sub(r'^(\s*)#progress\(', r'\1progress(', contents, flags=re.MULTILINE)
+    contents = re.sub(r'choosedowner\):', 'choosedowner, progress=gr.Progress()):', new_contents, flags=re.MULTILINE)
+
+    with open(batchlinks_dir, 'w') as f:
+        f.write(contents)
+
+else:
+    # os.system(f"sed -i 's/def run(command, choosedowner, progress=gr.Progress()):/def run(command, choosedowner):/g' {batchlinks_dir}")
+    # os.system(f"sed -i '/^progress(/ s/^/#/' {batchlinks_dir}")
+    new_contents = re.sub(r'^(\s*)progress\(', r'\1#progress(', contents, flags=re.MULTILINE)
+    contents = re.sub(r'choosedowner,\s*progress=gr\.Progress\(\)\):$', 'choosedowner):', new_contents, flags=re.MULTILINE)
+
+    with open(batchlinks_dir, 'w') as f:
+        f.write(contents)
